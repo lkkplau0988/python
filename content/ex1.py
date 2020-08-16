41dc3ae7263feaf4c315301fbf0cd2b1e67e6b2a
lkkplau0988 <lkkplau0988@gmail.com>
2020-08-17 02:44:04 +0800

    test from peter

--- content/ex1.py
+++ content/ex1.py
@@ -11,6 +11,7 @@ print( "This is fun.")
 print ('Yay! Printing.')
 print ("I'd much rather you 'not'.")
 print( 'I "said" do not touch this.')
+print('test from peter')
 
 # 输出中文的时候要在字符串前面加u。
 # 因为python中字符串默认采用ASCII，要声明为unicode就要加u(linux环境下不需要加u)
