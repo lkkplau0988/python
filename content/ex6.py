03074eaaa9ce6162aee18b327d976bb9629d59ef
lkkplau0988 <lkkplau0988@gmail.com>
2020-09-08 23:23:15 +0800

    modify ex6

--- content/ex6.py
+++ content/ex6.py
@@ -7,26 +7,26 @@
 # 输出文本的时候，最好用到用变量去代替，这样会剩下很多工作
 #
 
-x = "There are %d typs of people." % 10
+x = "There are %s typs of people." %"three"
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary,do_not)
 
-print x
-print y
+print(x)
+print(y)
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
+print("I said: %r." % x)
+print ("I also said: '%s'." % y)
 
 hilarious = False # 爆笑
 joke_evaluation = "Isn't that joke so funny?! %r"# 笑话评估
 
-print joke_evaluation % hilarious
+print(joke_evaluation % hilarious)
 
 w = "This is the left side of....."
 e = "a string with a right side."
 
-print w + e
+print (w + e)
 
 # 笔记
 #
